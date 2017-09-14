e8a884251debf8d95bd385eb915d5c58839e3021
Diego Moreno <A01022113@itesm.mx>
2017-09-14 10:25:31 -0500

    Segunda versión

--- Progra1.java
+++ Progra1.java
@@ -1,5 +1,6 @@
 public class Progra1{
     public static void main(String [] args){
-	System.out.println("Este es mi primer programa ");
+	System.out.println("Segundo programa  ");
+	System.out.println("Con este programa verificar si esta segunda versión se manda al repositorio creado");
     }
 }
